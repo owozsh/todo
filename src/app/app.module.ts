bc3149df6932cfe07578c509848f5158dd34285c
owozsh <accounts@owozsh.dev>
2021-11-10 14:57:39 -0300

    add sidebar and todo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
